51a9e77f7f4b5c602001e87f90ef86466eef55d8
Liam Silverman <liamsilvm@gmail.com>
2023-12-03 14:00:14 -0700

    testing 2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express() 
 const port = 3000 
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(cors)
+// app.use(cors)
 
 require('dotenv').config()
 const mysql = require('mysql2')
