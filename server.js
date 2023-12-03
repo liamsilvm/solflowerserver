bb7aa2e14265daa6be5723f33f227be2377238c4
Liam Silverman <liamsilvm@gmail.com>
2023-12-03 11:57:45 -0700

    connection to database

--- server.js
+++ server.js
@@ -2,10 +2,40 @@ const express = require('express')
 const app = express() 
 const port = 3000 
 
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+
+
+connection.connect((err) => { 
+    if(err){ 
+        console.error('error connecting to database', err.message); 
+        return; 
+    }
+    console.log('connected to database')
+})
 app.get('/', (req, res) =>  
     res.send('this is a test')
 )  
+app.get('/strains', (req, res) => { 
+    connection.query('SELECT * FROM strains', (queryError, results) => { 
+        if(queryError){ 
+            console.error('error querying', queryError.message)
+            return; 
+        }
 
+        res.json(results)
+    })
+})
+app.get('/pages', (req, res) => { 
+    connection.query('SELECT * FROM pages', (queryError, results) => { 
+        if(queryError){ 
+            console.error('error querying', queryError.message)
+        }
+        res.json(results)
+    })
+})
 app.listen(port, () => { 
     console.log('server is running on port ' + port)
 })
